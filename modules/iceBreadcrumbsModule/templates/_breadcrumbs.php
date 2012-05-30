a30a0a97e46692699f7271cb5a77db08e76bc39d
Yanko Simeonoff <yanko@zecho.info>
2012-05-30 11:01:49 +0300

    Changed text for every crumb is cut if exceeded given length

--- modules/iceBreadcrumbsModule/templates/_breadcrumbs.php
+++ modules/iceBreadcrumbsModule/templates/_breadcrumbs.php
@@ -10,11 +10,11 @@ foreach ($breadcrumbs as $key => $breadcrumb)
 {
   if ($url = $breadcrumb->getUri() and ++$index < $breadcrumbsCount)
   {
-    echo link_to($breadcrumb->getText(), $url, array('title'=> $breadcrumb->getTitle()));
+    echo link_to(truncate_text($breadcrumb->getText(), 40), $url, array('title'=> $breadcrumb->getTitle()));
   }
   else
   {
-    echo $breadcrumb->getText();
+    echo '<span title="', $breadcrumb->getText(), '">', truncate_text($breadcrumb->getText(), 40), '</span>';
   }
 
   if ($key < $breadcrumbsCount - 1)
